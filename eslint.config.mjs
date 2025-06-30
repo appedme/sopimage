0459e99ce2818b47d7ca9dcf51633711bed16699
SH20RAJ <shaswatraj3@gmail.com>
2025-06-30 12:42:43 +0530

    feat: update ESLint configuration to disable specific rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,20 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-console": "off",
+      "no-unused-vars": "off",
+      "no-debugger": "off",
+      "no-alert": "off",
+      "no-empty": "off",
+      "no-undef": "off",
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+
+    }
+  }
 ];
 
 export default eslintConfig;
